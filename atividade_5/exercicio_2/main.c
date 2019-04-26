e671e27ad7b712d1e1a3f2f9a567e8a593f1e63d
Alan Lüdke <alanludke@gmail.com>
2019-04-26 18:45:36 -0300

    corrigido ex2 para que seja explorado concorrencia e consumidores não sejam chutados se nao houver produtores

--- atividade_5/exercicio_2/main.c
+++ atividade_5/exercicio_2/main.c
@@ -11,11 +11,8 @@ void consumir(int produto); //< definida em helper.c
 void *produtor_func(void *arg);
 void *consumidor_func(void *arg);
 
-sem_t sem_produzir;
-sem_t sem_consumir;
-
-pthread_mutex_t mutex;
-
+sem_t sem_produzir,sem_consumir;
+pthread_mutex_t mutex_produtor, mutex_consumidor;
 int indice_produtor, indice_consumidor, tamanho_buffer;
 int* buffer;
 
@@ -33,10 +30,10 @@ void *produtor_func(void *arg) {
             produto = produzir(i); //produz um elemento normal
 
         sem_wait(&sem_produzir);
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex_produtor);
         indice_produtor = (indice_produtor + 1) % tamanho_buffer; //calcula posição próximo elemento
         buffer[indice_produtor] = produto; //adiciona o elemento produzido à lista
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex_produtor);
         sem_post(&sem_consumir);
     }
     return NULL;
@@ -45,19 +42,15 @@ void *produtor_func(void *arg) {
 void *consumidor_func(void *arg) {
     while (1) {
         sem_wait(&sem_consumir);
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex_consumidor);
         indice_consumidor = (indice_consumidor + 1) % tamanho_buffer; //Calcula o próximo item a consumir
         int produto = buffer[indice_consumidor]; //obtém o item da lista
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex_consumidor);
         sem_post(&sem_produzir);
 
         //Podemos receber um produto normal ou um produto especial
         if (produto >= 0){
-            if(buffer != NULL){
-              consumir(produto); //Consome o item obtido.
-            } else {
-              //wait();
-            }
+            consumir(produto); //Consome o item obtido.
         } else
             break; //produto < 0 é um sinal de que o consumidor deve parar
     }
@@ -74,30 +67,26 @@ int main(int argc, char *argv[]) {
     int itens = atoi(argv[2]);
     int n_produtores = atoi(argv[3]);
     int n_consumidores = atoi(argv[4]);
+    int consumidores_extras = n_consumidores - n_produtores;
+
     printf("itens=%d, n_produtores=%d, n_consumidores=%d\n",
 	   itens, n_produtores, n_consumidores);
 
     //Iniciando buffer
     indice_produtor = 0;
-    indice_consumidor = 0;
     buffer = malloc(sizeof(int) * tamanho_buffer);
+    indice_consumidor = 0;
 
     // Crie threads e o que mais for necessário para que n_produtores
     // threads criem cada uma n_itens produtos e o n_consumidores os
     // consumam.
 
     // ....
-    pthread_mutex_init(&mutex, NULL); // Inicializa o mutex destravado
-
+    pthread_mutex_init(&mutex_produtor, NULL); // Inicializa o mutex destravado
+    pthread_mutex_init(&mutex_consumidor, NULL); // Inicializa o mutex destravado
 
     pthread_t threads_produtores[n_produtores];
-
-    if (n_consumidores > n_produtores) {
-        n_consumidores = n_produtores;
-    }
-
     pthread_t threads_consumidores[n_consumidores];
-
 
     sem_init(&sem_produzir, 0, tamanho_buffer);
     sem_init(&sem_consumir, 0, 0);
@@ -114,11 +103,21 @@ int main(int argc, char *argv[]) {
       pthread_join(threads_produtores[i], NULL);
     }
 
+    if (consumidores_extras > 0) {
+        for (int i = 0; i < consumidores_extras; i++) {
+            sem_wait(&sem_produzir);
+            indice_produtor = (indice_produtor+1) % tamanho_buffer; //Calcula o próximo item a consumir
+            buffer[indice_produtor] = -1; //obtém o item da lista
+            sem_post(&sem_consumir);
+        }
+    }
+
     for (size_t i = 0; i < n_produtores; i++) {
       pthread_join(threads_consumidores[i], NULL);
     }
 
-    pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&mutex_produtor);
+    pthread_mutex_destroy(&mutex_consumidor);
 
     sem_destroy(&sem_produzir);
     sem_destroy(&sem_consumir);
